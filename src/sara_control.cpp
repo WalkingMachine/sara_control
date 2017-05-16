863f055121c4a0b86ed39ca6a9df8e9518dc6ea5
thierry pouplier <thierrypouplier@gmail.com>
2017-05-16 02:30:19 -0400

    Build. fix namespace error

--- src/sara_control.cpp
+++ src/sara_control.cpp
@@ -3,12 +3,13 @@
 //
 #include <ros/ros.h>
 #include <controller_manager/controller_manager.h>
-#include <elmec_robot_control/elmec_hardware.h>
+#include <sara_control/sara_control.h>
+#include <combined_robot_hw/combined_robot_hw.h>
 
 
-using namespace sara_control;
+using namespace sara_control_ns;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "sara_control");
 
@@ -18,16 +19,17 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   combined_robot_hw::CombinedRobotHW chw;
 
+  chw.init(nh, nh);
+
   controller_manager::ControllerManager cm(&chw, nh);
 
   ros::Duration period(0.02);
 
-  while (ros::ok())
-  {
-    chw.read();
+  while (ros::ok()) {
+    chw.read(ros::Time::now(), period);
     cm.update(ros::Time::now(), period);
-    chw.write();
+    chw.write(ros::Time::now(), period);
     period.sleep();
   }
   return 0;
-  }
+}
