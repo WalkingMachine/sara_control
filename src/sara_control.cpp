3e588e76616b58921630e8000fd3551a4c889ba9
thierry pouplier <thierrypouplier@gmail.com>
2017-05-28 20:20:23 -0400

    Commenting e-stop stuff for now

--- src/sara_control.cpp
+++ src/sara_control.cpp
@@ -6,7 +6,6 @@
 #include <sara_control/sara_control.h>
 #include <combined_robot_hw/combined_robot_hw.h>
 
-
 using namespace sara_control_ns;
 
 int main(int argc, char **argv)
@@ -22,6 +21,13 @@ int main(int argc, char **argv)
   chw.init(nh, nh);
 
   controller_manager::ControllerManager cm(&chw, nh);
+
+  /// Commented in wait of the e-stop integration
+  //std::string moveArmActionName = "/sara_arm_driver_node/execute_plan";
+  //nh.param("/sara_supervisor_node/move_arm_action_name", moveArmActionName, moveArmActionName);
+
+  //std::string moveBaseActionName = "/move_base";
+  //nh.param("/sara_supervisor_node/move_base_action_name", moveBaseActionName, moveBaseActionName);
 
   ros::Duration period(0.02);
 
