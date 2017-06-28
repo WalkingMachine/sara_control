668de8004ad5542534279edc7f7af6dbdfc7a512
thierry pouplier <thierrypouplier@gmail.com>
2017-06-28 01:34:47 -0400

    Time to go to bed..launching for real now

--- src/sara_control.cpp
+++ src/sara_control.cpp
@@ -13,30 +13,19 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "sara_control");
   ros::NodeHandle nh;
 
-  ros::Subscriber eStopSub = nh.subscribe("start_button_msg", 1, &eStopCallback);
-  stopSignalSrv_ = nh_.advertiseService("safety_stop_srv", &wmSupervisor::stopSignalService, this);
-
   combined_robot_hw::CombinedRobotHW chw;
 
   chw.init(nh, nh);
 
   controller_manager::ControllerManager cm(&chw, nh);
 
-
   ros::Duration period(0.02);  // 50 Hz
 
   while (ros::ok())
   {
-    ROS_DEBUG("Test");
     chw.read(ros::Time::now(), period);
-    if (ESTOP || last_tic + 0.5 > ros::Time::now())
-    {
-      cm.update(ros::Time::now(), period, true);
-    }
-    else
-    {
-      cm.update(ros::Time::now(), period, false);
-    }
+    //cm.update(ros::Time::now(), period, true);
+    cm.update(ros::Time::now(), period, false);
     chw.write(ros::Time::now(), period);
     period.sleep();
   }
