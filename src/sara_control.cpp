a7e57518ec0353cc957193a7fd05b5188103c977
philippe117 <philippe117@live.fr>
2017-07-16 20:28:12 -0400

    fix control loop

--- src/sara_control.cpp
+++ src/sara_control.cpp
@@ -5,49 +5,48 @@
 #include <controller_manager/controller_manager.h>
 #include <combined_robot_hw/combined_robot_hw.h>
 #include <realtime_tools/realtime_publisher.h>
-
+#include <std_srvs/SetBool.h>
+#include <std_msgs/Bool.h>
 
 bool ESTOP;
 
-bool callback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response)
-{
-  ESTOP = request.data;
-  reponse.success = true;
-  return true;
+bool callback(std_srvs::SetBool::Request &request, std_srvs::SetBool::Response &response) {
+    ESTOP = request.data;
+    response.success = true;
+    return true;
 }
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "sara_control");
-  ros::NodeHandle nh;
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "sara_control");
+    ros::NodeHandle nh;
 
-  ros::ServiceServer status_service = nh.advertiseService("estop_service", callback);
+    ros::ServiceServer status_service = nh.advertiseService("estop_service", callback);
 
-  realtime_tools::RealtimePublisher rt_pub = realtime_tools::RealtimePublisher<std_msgs::Bool>(nh, "estop_status", 4);
+    realtime_tools::RealtimePublisher<std_msgs::Bool> *rt_pub;
 
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+    rt_pub = new realtime_tools::RealtimePublisher<std_msgs::Bool>(nh, "estop_status", 4);
 
-  combined_robot_hw::CombinedRobotHW chw;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
-  chw.init(nh, nh);
+    combined_robot_hw::CombinedRobotHW chw;
 
-  controller_manager::ControllerManager cm(&chw, nh);
+    chw.init(nh, nh);
 
-  ros::Duration period(0.02);  // 50 Hz
+    controller_manager::ControllerManager cm(&chw, nh);
 
-  while (ros::ok())
-  {
-    chw.read(ros::Time::now(), period);
-    cm.update(ros::Time::now(), period, !ESTOP);
-    chw.write(ros::Time::now(), period);
-    if(rt_pub->trylock())
-    {
-      realtime_pub->msg_ = ESTOP;
-      realtime_pub->unlockAndPublish();
+    ros::Duration period(0.02);  // 50 Hz
+
+    while (ros::ok()) {
+        chw.read(ros::Time::now(), period);
+        cm.update(ros::Time::now(), period, !ESTOP);
+        chw.write(ros::Time::now(), period);
+        if (rt_pub->trylock()) {
+            rt_pub->msg_.data = (char)(ESTOP ? 1:0);
+            rt_pub->unlockAndPublish();
+        }
+        period.sleep();
     }
-    period.sleep();
-  }
-  
-  return 0;
+    free(rt_pub);
+    return 0;
 }
